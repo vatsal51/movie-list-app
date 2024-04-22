f49243abea15e9ebde72f57309246d043fa32815
VATSAL PATEL <vatsal539241@gmail.com>
2024-04-23 01:17:00 +0530

    minor cahnge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
-import "../globals.css";
+import "./globals.css";
 function App() {
   return (
     <div className="App">
